0f07d53d86fe7cc8368bde075a88ddfed799af9c
Jerry Wu <jw910731@gmail.com>
2019-03-14 15:33:35 +0800

    Compile script supports cross platform ( not tested )

--- compile.sh
+++ compile.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-#$1 = source
-#$2 = destination
-#$3 = mode
+#$1 = platform
+#$2 = source
+#$3 = destination
+#$4 = mode
 args=('-Wall' '-Wextra' '-O2' '-std=c++14' '-fdiagnostics-color=auto' '-fno-diagnostics-show-option' '-fno-diagnostics-show-caret' '-Wno-unused-result')
-if [[ $3 -eq 'Test' ]];then
+if [[ $4 -eq 'Test' ]];then
 	args+=('-DTEST')
 fi
-args+=($1 '-o' $2)
+args+=($2 '-o' $3)
+cmd='g++'
+if [[ $1 -eq 'osx' ]];then
+    cmd='g++-8'
+fi
 command "g++" "${args[@]/#}"
 exit

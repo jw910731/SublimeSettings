7d7b4077bfc4921e3ab44307f403cda5520f2687
Jerry Wu <jw910731@gmail.com>
2019-03-20 22:57:19 +0800

    Bug fix compile code mode change

--- compile.sh
+++ compile.sh
@@ -7,7 +7,7 @@
 #Set default compile flag
 args=('-Wall' '-Wextra' '-O2' '-std=c++14' '-fdiagnostics-color=auto' '-fno-diagnostics-show-option' '-fno-diagnostics-show-caret' '-Wno-unused-result')
 #If option Test is destinate
-if [[ $3 -eq 'Test' ]];then
+if [[ $3 == "Test" ]];then
 	args+=('-DTEST')
 fi
 #add source and ouput to compile flag

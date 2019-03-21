2378cb7f1d79064c87df349169938367eb67314b
Jerry Wu <jw910731@gmail.com>
2019-03-22 00:19:17 +0800

    Fix compile error while path includes space

--- compile.sh
+++ compile.sh
@@ -11,7 +11,7 @@ if [[ $3 == "Test" ]];then
 	args+=('-DTEST')
 fi
 #add source and ouput to compile flag
-args+=($1 '-o' $2)
+args+=("$1" '-o' "$2")
 #If system is OSX => use gcc instead of clang
 if [[ $OSTYPE == 'darwin'* ]];then 
     cmd='g++-8'

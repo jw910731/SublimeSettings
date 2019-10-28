aaa14ef90ca3659dd37f1f0f41a16bdb974c1944
Jerry Wu <jw910731@gmail.com>
2019-10-28 14:59:46 +0800

    add return code display

--- launch-wrapper.sh
+++ launch-wrapper.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 "$1"
+ret_code=$?
 echo -e '\n=============================='
+echo -e "Program exited with return code \u001b[32m$ret_code\u001b[0m.\n"
 read -n 1 -s -r -p "Press any key to end your code"
 echo -e '\n'
